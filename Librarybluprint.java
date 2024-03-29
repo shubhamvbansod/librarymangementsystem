35a306b675a336701ac4b5a2b8579c9a75f162a1
Shubham Vishwanath Bansod <160355664+shubhamvbansod@users.noreply.github.com>
2024-03-29 21:53:06 +0530

    Create Librarybluprint.java

---
+++ Librarybluprint.java
@@ -0,0 +1,81 @@
+package jdbc_library_manegment_system;
+
+public class Library {
+	
+	private int id ;
+	private String name ;
+	private String email;
+	private long phone ;
+	private String password ;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+	
+	private int bId ;
+	private String bName ;
+	private String outher ;
+	private String type ;
+
+
+	public int getbId() {
+		return bId;
+	}
+	public void setbId(int bId) {
+		this.bId = bId;
+	}
+	public String getbName() {
+		return bName;
+	}
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+	public String getOuther() {
+		return outher;
+	}
+	public void setOuther(String outher) {
+		this.outher = outher;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+	
+
+}
